125baa459e9b79d7f6daea706ab75816c825887f
Shyam Panchal <shyampanchal95@gmail.com>
2018-12-08 12:43:52 -0500

    .

---
+++ TankUnknowns/TankUnknown's Battleground/Assets/Scripts/BulletShoot.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BulletShoot : NetworkBehaviour
+{
+
+    public GameObject bulletPrefab;
+
+    public GameObject bulletSpawnPoint;
+
+    public int bulletSpeed;
+
+    public float destroyTime = 2.0f;
+
+    public bool canFire;
+
+    public float fireTimer;
+
+    // Use this for initialization
+    void Start()
+    {
+        canFire = true;
+        fireTimer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(hasAuthority == false)
+        {
+            return;
+        }
+
+        float fire = Input.GetAxis("Fire1");
+        if (canFire)
+        {
+            if (fire > 0)
+            {
+                CmdShootBullet();
+                canFire = false;
+            }
+        }
+        else
+        {
+            fireTimer += Time.deltaTime;
+            if (fireTimer >= 0.4f)
+            {
+                fireTimer = 0.0f;
+                canFire = true;
+            }
+
+        }
+
+    }
+
+    public void Fire()
+    {
+        var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.transform.position, new Quaternion(0, 90, 90, 0));
+
+        bullet.GetComponent<Rigidbody>().velocity = this.transform.forward * bulletSpeed;
+
+        Destroy(bullet, destroyTime);
+    }
+
+    ////////////////////// COMMANDS
+    ///
+
+    [Command]
+    void CmdShootBullet()
+    {
+
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.transform.position, new Quaternion(0, 90, 90, 0));
+
+        bullet.GetComponent<Rigidbody>().velocity = this.transform.forward * bulletSpeed;
+
+        NetworkServer.Spawn(bullet);
+
+        Destroy(bullet, destroyTime);
+    }
+}
---
+++ TankUnknowns/TankUnknown's Battleground/Assets/Scripts/TankController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TankController : NetworkBehaviour {
+    Rigidbody rb;
+    public int speed;
+    Vector3 m_EulerAngleVelocity;
+
+    public GameObject mainCamera;
+    public GameObject spotLight;
+
+    public GameObject mapCamera;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        mapCamera = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(hasAuthority == false)
+        {
+            return;
+        }
+
+        Debug.Log("Main Camera Set Active ... ");
+        mainCamera.SetActive(true);
+        spotLight.SetActive(true);
+        mapCamera.SetActive(false);
+
+        float y = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+
+        rb.velocity = transform.forward * y * speed;
+        m_EulerAngleVelocity = new Vector3(0, x*100, 0);
+        Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+    }
+}
